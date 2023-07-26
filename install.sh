c4478bda7a19d9f746045a37d7785115c1e05146
22 <leogcry22@users.noreply.github.com>
2023-07-26 16:39:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ trap report_error ERR
 
 
 mkdir -p ~/Applications
-curl -L "https://github.com/EmuDeck/emudeck-electron/releases/download/v2.1.4/EmuDeck-2.1.4.AppImage" -o ~/Applications/EmuDeck.AppImage 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\n#Download Speed\:\2/' | zenity --progress --title "Downloading EmuDeck" --width 600 --auto-close --no-cancel 2>/dev/null
+curl -L "https://ghproxy.com/https://github.com/EmuDeck/emudeck-electron/releases/download/v2.1.4/EmuDeck-2.1.4.AppImage" -o ~/Applications/EmuDeck.AppImage 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\n#Download Speed\:\2/' | zenity --progress --title "Downloading EmuDeck" --width 600 --auto-close --no-cancel 2>/dev/null
 chmod +x ~/Applications/EmuDeck.AppImage
 ~/Applications/EmuDeck.AppImage
